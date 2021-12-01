08566f7dcdbe00f9b6313e0ab5e44a939457ce4d
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-11-30 22:58:26 -0600

    added-compo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import Header from '../compo/Header';
+import Footer from '../compo/Footer'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Header/>
+      <Component 
+      {...pageProps} 
+      />
+     <Footer/>
+    </div>
+  
+  )
 }
 
 export default MyApp
